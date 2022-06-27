0d8badcf5666837bd5f0f76a99675715cb1db45a
Martin Feckie <martin.feckie@3plearning.com>
2022-06-27 13:33:47 +0800

    Convert to task and update tests

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,9 +1,16 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default class BooksRoute extends Route {
   @service store;
   model() {
+    this.bookFinder.perform();
+    return this.bookFinder;
+  }
+
+  @task
+  async bookFinder() {
     return this.store.findAll('book');
   }
 }
--- tests/integration/components/book-list-test.js
+++ tests/integration/components/book-list-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'testing-examples/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, waitFor } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { fakeTask } from '../../helpers/fake-task';
 
 module('Integration | Component | book-list', function (hooks) {
   setupRenderingTest(hooks);
@@ -13,13 +14,63 @@ module('Integration | Component | book-list', function (hooks) {
   });
 
   test('it renders a list', async function (assert) {
-    this.books = [
+    this.books = fakeTask(() => [
       { name: 'A Tale of Two Cities' },
       { name: 'Great Expectations' },
       { name: 'Oliver Twist' },
-    ];
+    ]);
+
+    this.books.perform();
+
     await render(hbs`<BookList @books={{this.books}} />`);
 
     assert.dom('li').exists({ count: 3 });
+  });
+
+  test('it shows a loading spinner when loading', async function (assert) {
+    this.books = { isRunning: true };
+
+    await render(hbs`<BookList @books={{this.books}} />`);
+
+    assert.dom('div.loading').exists();
+  });
+
+  test('Can model spinner and resolution with fakeTask', async function (assert) {
+    let resolver;
+
+    this.books = fakeTask(() => {
+      return new Promise((resolve) => {
+        resolver = resolve;
+      });
+    });
+
+    this.books.perform();
+
+    await render(hbs`<BookList @books={{this.books}} />`);
+
+    assert.dom('div.loading').exists();
+
+    resolver([
+      { name: 'A Tale of Two Cities' },
+      { name: 'Great Expectations' },
+    ]);
+
+    await waitFor('li');
+
+    assert.dom('div.loading').doesNotExist();
+    assert.dom('li').exists({ count: 2 });
+  });
+
+  test('it shows a loading spinner when loading', async function (assert) {
+    this.books = fakeTask(() => {
+      return Promise.reject();
+    });
+
+    // Catching error here because otherwise it causes a global failure in the test suite
+    this.books.perform().catch();
+
+    await render(hbs`<BookList @books={{this.books}} />`);
+
+    assert.dom('div.error').exists();
   });
 });
