55719ed873d0cfe8832245d2ae93e4a8e78121be
Martin Feckie <martin.feckie@3plearning.com>
2022-06-27 12:25:18 +0800

    Add test for list

--- tests/integration/components/book-list-test.js
+++ tests/integration/components/book-list-test.js
@@ -11,4 +11,15 @@ module('Integration | Component | book-list', function (hooks) {
 
     assert.dom('li').hasText('Nothing to see here');
   });
+
+  test('it renders a list', async function (assert) {
+    this.books = [
+      { name: 'A Tale of Two Cities' },
+      { name: 'Great Expectations' },
+      { name: 'Oliver Twist' },
+    ];
+    await render(hbs`<BookList @books={{this.books}} />`);
+
+    assert.dom('li').exists({ count: 3 });
+  });
 });
