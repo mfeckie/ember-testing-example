a787e80225ac0470ad8a5e005dcd3e2d18332780
Martin Feckie <martin.feckie@3plearning.com>
2022-06-27 11:30:04 +0800

    Nothing to see here

--- tests/integration/components/book-list-test.js
+++ tests/integration/components/book-list-test.js
@@ -6,9 +6,9 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | book-list', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders an empty message', async function (assert) {
     await render(hbs`<BookList />`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom('li').hasText('Nothing to see here');
   });
 });
